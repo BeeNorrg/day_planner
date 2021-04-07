eb5055f7cac8aff90b0eb40485fcba151337b3de
BeeNorrg <brandon.n.norrgard@gmail.com>
2021-04-07 12:30:15 -0500

    js now dynamically creates the work day schedule

--- assets/planner_script.js
+++ assets/planner_script.js
@@ -1,5 +1,5 @@
 // current day/hour in moment.js
-let hour = moment().format("H")
+let hour = moment().format("H");
 let day = moment().format("dddd, MMMM Do");
 
 const dayDiv = $("#currentDay");
@@ -57,21 +57,23 @@ let times = JSON.parse(localStorage.getItem("plannerEvents")) ||
 function plannerMaker() {
     console.log("times.length:", times.length);
     for(i=0; i < times.length; i++) {
-        $("#loadRows").append("<div class='row'></div>");
-        $(".row").append("<div class='col-1 time'></div>");
-        $(".time").text(times.hour);
-        //adds text area for users to add events or puts an event in the div if there is one
-        $(".row").append("<textarea class='col'>" + times.event + "</textarea>")
-        $(".row").append("<button type='button' class='btn'><i class='fas fa-save'></i></button>")
-        
-        if (hour == times.dataHour) {
-            $(timeEl).attr("class", "present col");
-        } else if (hour < times.dataHour) {
-            $(timeEl).attr("class", "past col");
-        } else if (hour > times.dataHour) {
-            $(timeEl).attr("class", "future col");
+        $("#loadRows").append("<div class='row' id='row" + i + "'></div>");
+        let userInput = $("<textarea class='col'>" + times[i].event + "</textarea>")
+        $("#row" + i).append("<div class='col-1 time'>" + times[i].hour + "</div>");
+        //styles the textarea being added depending on whether it's before or after the current hour
+        if (hour == times[i].dataHour) {
+            $(userInput).attr("class", "present col");
+        } else if (hour < times[i].dataHour) {
+            $(userInput).attr("class", "past col");
+        } else if (hour > times[i].dataHour) {
+            $(userInput).attr("class", "future col");
         }
-    }
+        console.log("times.dataHour:", times.dataHour);
+         //adds text area for users to add events or puts an event in the div if there is one
+        $("#row" + i).append(userInput)
+        $("#row" + i).append("<button type='button' class='btn'><i class='fas fa-save'></i></button>")
+        
+    };
 };
     
 
